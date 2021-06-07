1431bc574c3a065d35d7e76b78492fc3769fd7b0
Colleen O'Briant <cobriant@uoregon.edu>
2021-06-06 19:31:11 -0700

    arrange documentation test

--- R/funs.R
+++ R/funs.R
@@ -1,1 +1,26 @@
+#' Arrange rows of a tibble
+#'
+#' Orders rows of a tibble by the variable of your choice.
+#'
+#' @usage
+#'
+#' arrange(data, ...)
+#'
+#' @examples
+#' msleep %>% arrange(sleep_total)
+#'
+#' msleep %>% arrange(desc(sleep_total))
+#'
+#' @seealso
+#'
+#' select
+#'
+#' group_by
+#'
+#' summarize
+#'
+#' mutate
+#'
+#' filter
+#'
 arrange <- function(){}
