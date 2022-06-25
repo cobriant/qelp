59117d1769cc90940ef0b5db799221edd666df47
Colleen O'Briant <59625715+cobriant@users.noreply.github.com>
2022-06-25 14:09:59 -0700

    Create round.R

---
+++ R/round.R
@@ -0,0 +1,42 @@
+#' Round
+#'
+#' @description
+#'
+#' Round to the nearest integer.
+#'
+#' @details
+#'
+#' `round()`
+#'
+#' @usage
+#'
+#' @examples
+#'
+#' # By default, `round()` rounds to the nearest integer:
+#'
+#' round(1.2)
+#'
+#' #> [1] 1
+#'
+#' -----------------------------------
+#'
+#' # You can specify the number of decimal places to round to:
+#'
+#' round(1.23456, 3)
+#'
+#' #> 1.235
+#'
+#' -----------------------------------
+#'
+#' # `round()` is vectorized:
+#'
+#' round(1:5 / 3, 3)
+#'
+#' #> 0.333 0.667 1.000 1.333 1.667
+#'
+#' 
+#' @export
+#' @seealso
+#' 
+#' 
+round <- function(){}
